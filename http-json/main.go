3bdcde320cdc11bb2e5d926246b9ec5496c9afb2
Mark <mark.day@aistemos.com>
2021-12-19 13:12:29 +0000

    initial commit

--- http-json/main.go
+++ http-json/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
+type people struct {
+	Number int `json:"number"`
+}
+
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Starting")
+	url := "http://api.open-notify.org/astro.json"
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, err := spaceClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := people{}
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(p.Number)
+
 }
